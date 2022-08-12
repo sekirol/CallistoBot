098b77902a9095c0f3cb2e4bbd8c2bc78a4cc5ae
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-12 07:31:31 +0300

    Vk session class implementation

---
+++ vk_session.py
@@ -0,0 +1,18 @@
+
+from vk_api import VkApi
+
+from tools import get_access_data
+
+class VkSession():
+    def __init__(self):
+        self._connect()
+
+    def _connect(self):
+        access = get_access_data("vk")
+        session = VkApi(access['login'], access['password'])    
+    
+        session.auth()
+        self.api = session.get_api()
+
+    def get_user_data(self):
+        return self.api.users.get()
--- worker.py
+++ worker.py
@@ -1,15 +1,9 @@
 
-from vk_api import VkApi
-
-from tools import get_access_data
 from bot_engine import TgBotEngine
+from vk_session import VkSession
 
 def main():
-    vk_access = get_access_data("vk")
-    vk_session = VkApi(vk_access['login'], vk_access['password'])    
-    
-    vk_session.auth()
-    vk_api = vk_session.get_api()
+    vk_session = VkSession()
 
     bot_engine = TgBotEngine()
     bot_engine.start()
