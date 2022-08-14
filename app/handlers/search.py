7e545c20489f89674cbd4567ddc03ac96fdbb335
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-15 01:21:08 +0300

    State duration class renamed

--- app/handlers/search.py
+++ app/handlers/search.py
@@ -3,14 +3,12 @@ from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from datetime import datetime
 
-from .state_machine import AppState
+from .state_machine import AppState, StateContext
 
 async def cmd_search(message: types.Message, state: FSMContext):
     await state.set_state(AppState.search)
     
-    context = {
-        "state_last_activity": datetime.now().isoformat(sep=" ")
-    }
+    context = StateContext()
     await state.set_data(context)
     
     await message.answer("<b>Поиск vk.com</b>\n\n"
@@ -19,8 +17,9 @@ async def cmd_search(message: types.Message, state: FSMContext):
 
 async def search_state_handler(message: types.Message, state: FSMContext):
     context = await state.get_data()
-    session_time = datetime.now() - datetime.fromisoformat(context["state_last_activity"])
-    await message.answer(f"{session_time}")
+    
+    await message.answer(f"Last activity time: {context.get_last_activity()}. " \
+                         f"State is {'active' if context.is_state_active() else 'inactive'}.")
 
 def register_handlers_search(dp: Dispatcher):
     dp.register_message_handler(cmd_search, commands="search", state=AppState.initial)
--- app/handlers/state_machine.py
+++ app/handlers/state_machine.py
@@ -1,6 +1,5 @@
 
 from aiogram.dispatcher.filters.state import StatesGroup, State
-
 from datetime import datetime, timedelta
 
 INACTIVE_SESSION_MAX_DURATION = 5
@@ -9,18 +8,18 @@ class AppState(StatesGroup):
     initial = State()
     search = State()
 
-class CurrentSession:
+class StateContext:
     def __init__(self):
+        self._last_activity_time = None
+
         self.update_activity_time()
 
-    def __str__(self):
-        session_duration = (datetime.now() - self._last_activity_time)
-        return f"Session lasts {session_duration.seconds} second. " \
-               f"Session is {'active' if self.is_session_active() else 'inactive'}."
+    def get_last_activity(self) -> str:
+        return self._last_activity_time.isoformat(sep=" ", timespec="seconds")
 
     def update_activity_time(self):
         self._last_activity_time = datetime.now()
 
-    def is_session_active(self) -> bool:
+    def is_state_active(self) -> bool:
         session_duration = datetime.now() - self._last_activity_time
         return session_duration < timedelta(minutes=INACTIVE_SESSION_MAX_DURATION)
