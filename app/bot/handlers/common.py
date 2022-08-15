cda177fe364d657d48d50e18cd18a97e4db15fbe
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-15 20:30:47 +0300

    Cancel command restarts bot

--- app/bot/handlers/common.py
+++ app/bot/handlers/common.py
@@ -6,14 +6,11 @@ from aiogram.dispatcher.filters import Text
 from ..state_machine import AppState
 
 async def cmd_start(message: types.Message, state: FSMContext):
-    await AppState.initial.set()
-    await message.answer("Приветствую!")
-
-async def cmd_cancel(message: types.Message, state: FSMContext):
     await state.finish()
-    await message.reply('Действие отменено', reply_markup=types.ReplyKeyboardRemove())
+    await state.set_state(AppState.initial)
+    await message.answer("Приветствую!", reply_markup=types.ReplyKeyboardRemove())
 
 def register_handlers_common(dp: Dispatcher):
     dp.register_message_handler(cmd_start, commands="start", state="*")
-    dp.register_message_handler(cmd_cancel, commands="cancel", state="*")
-    dp.register_message_handler(cmd_cancel, Text(equals="отмена", ignore_case=True), state="*")
+    dp.register_message_handler(cmd_start, commands="cancel", state="*")
+    dp.register_message_handler(cmd_start, Text(equals="отмена", ignore_case=True), state="*")
