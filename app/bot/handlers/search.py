f5edb233f0588cc0857fb174d08a0f0c706171d6
Sergei Kirikeza <serj280294@yandex.ru>
2022-08-16 16:28:01 +0300

    State context class was removed

--- app/bot/handlers/search.py
+++ app/bot/handlers/search.py
@@ -1,25 +1,18 @@
 
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
-from datetime import datetime
 
-from ..state_machine import AppState, StateContext
+from ..state_machine import AppState
 
 async def cmd_search(message: types.Message, state: FSMContext):
     await state.set_state(AppState.search)
-    
-    context = StateContext()
-    await state.set_data(context)
-    
+
     await message.answer("<b>Поиск vk.com</b>\n\n"
                          "Введите поисковый запрос:",
                          parse_mode="HTML")
 
 async def search_state_handler(message: types.Message, state: FSMContext):
-    context = await state.get_data()
-    
-    await message.answer(f"Last activity time: {context.get_last_activity()}. " \
-                         f"State is {'active' if context.is_state_active() else 'inactive'}.")
+    await message.answer("Ok")
 
 def register_handlers_search(dp: Dispatcher):
     dp.register_message_handler(cmd_search, commands="search", state=AppState.initial)
--- app/bot/state_machine.py
+++ app/bot/state_machine.py
@@ -1,25 +1,8 @@
 
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from datetime import datetime, timedelta
 
 INACTIVE_SESSION_MAX_DURATION = 5
 
 class AppState(StatesGroup):
     initial = State()
     search = State()
-
-class StateContext:
-    def __init__(self):
-        self._last_activity_time = None
-
-        self.update_activity_time()
-
-    def get_last_activity(self) -> str:
-        return self._last_activity_time.isoformat(sep=" ", timespec="seconds")
-
-    def update_activity_time(self):
-        self._last_activity_time = datetime.now()
-
-    def is_state_active(self) -> bool:
-        session_duration = datetime.now() - self._last_activity_time
-        return session_duration < timedelta(minutes=INACTIVE_SESSION_MAX_DURATION)
